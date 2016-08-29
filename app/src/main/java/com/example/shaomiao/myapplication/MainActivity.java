748a4cb87e582b5d94c45b44f73767108f20a7b5
shaomiao <shaomiaojava@163.com>
2016-08-29 17:25:14 +0800

    two

--- app/src/main/java/com/example/shaomiao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shaomiao/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.shaomiao.myapplication;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Camera;
 import android.graphics.Color;
 import android.graphics.Matrix;
@@ -14,35 +16,47 @@ import android.view.animation.Transformation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import junit.framework.Test;
+
 import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv=(ImageView) findViewById(R.id.image);
+        iv.setOnClickListener(this);
     }
 
-    public void onClickAnimation(final View view) {
+    public void onClickAnimation(View view) {
         //属性动画
-        ObjectAnimator anim=ObjectAnimator
-                .ofFloat(view, "rotationY", 0.0F, 360.0F)
-                .setDuration(1000);
-        anim.start();
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
-        {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation)
-            {
-                float cVal = (Float) animation.getAnimatedValue();
-                view.setAlpha(cVal);
-                view.setScaleX(cVal);
-                view.setScaleY(cVal);
-            }
-        });
+//        ObjectAnimator anim=ObjectAnimator
+//                .ofFloat(view, "rotationY", 0.0F, 360.0F)
+//                .setDuration(1000);
+//        anim.start();
+        Intent intent=new Intent(MainActivity.this,TestAcitvity.class);  //方法1
+        startActivity(intent);
+        //安卓组合动画
+//        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+//        {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation)
+//            {
+//                float cVal = (Float) animation.getAnimatedValue();
+//                view.setAlpha(cVal);
+//                view.setScaleX(cVal);
+//                view.setScaleY(cVal);
+//            }
+//        });
 
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(this, TestAcitvity.class);
+        startActivity(intent);
+    }
 }
 
 
---
+++ app/src/main/java/com/example/shaomiao/myapplication/TestAcitvity.java
@@ -0,0 +1,15 @@
+package com.example.shaomiao.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shaomiao on 2016-8-29.
+ */
+public class TestAcitvity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_layout);
+    }
+}
